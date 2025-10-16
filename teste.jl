c1c27d5962efcb7c57690809b73f6dcb1c43234f
celiszin <teste153ovo@gmail.com>
2025-10-16 15:03:26 -0300

    first commit

---
+++ teste.jl
@@ -0,0 +1,10 @@
+x = 12
+
+println("Coloca a porra do nome: ")
+nome = readline()
+
+if x > 11
+    while true
+        println(nome, "Fudido")
+    end
+end
---
+++ teste2.jl
@@ -0,0 +1,4 @@
+println("Qual seu nome, filho da puta? ")
+nome = readline()
+
+println(nome)
